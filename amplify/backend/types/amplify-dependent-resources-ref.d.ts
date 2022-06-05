df84d63565e153bef79669400a09f6257908a6f1
Huang <hruixin@88665a07b4b5.ant.amazon.com>
2022-06-05 13:00:18 +1000

    2nd commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "AdminsGroupRole": "string",
+            "PhotographerGroupRole": "string"
+        },
+        "case1010118337107dcbfe407dcbfe4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "GoogleWebClient": "string",
+            "FacebookWebClient": "string"
+        }
+    },
+    "storage": {
+        "s36ccda3a3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "case1010118337107dcbfe407dcbfe4PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    }
+}
