1e8b2fa1566cef873f6885d1bb6b81a60b3910bc
Huang <hruixin@88665a07b4b5.ant.amazon.com>
2022-06-08 16:38:43 +1000

    add auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,23 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "userPoolGroups": {
-            "AdminsGroupRole": "string",
-            "PhotographerGroupRole": "string"
-        },
-        "case1010118337107dcbfe407dcbfe4": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "GoogleWebClient": "string",
-            "FacebookWebClient": "string"
-        }
-    },
     "storage": {
         "s36ccda3a3": {
             "BucketName": "string",
